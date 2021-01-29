f09ad760ce85d1d5689ddb435005dcfa011e28ad
Preston <pjputzel@yahoo.com>
2021-01-28 18:37:43 -0800

    pre merge commit

--- src/main.py
+++ src/main.py
@@ -66,11 +66,10 @@ if __name__ == '__main__':
 
 
     
-#    path_to_config = '../configs/linear_baseline_configs/covid_configs/linear_delta_per_step_covid.yaml'
 #    path_to_config = '../configs/linear_baseline_configs/covid_configs/linear_delta_per_step_covid_num_events_only.yaml'
 #    path_to_config = '../configs/RNN_based_model_configs/synth_configs/RNN_delta_per_step.yaml'
 #    path_to_config = '../configs/RNN_based_model_configs/dm_cvd_configs/RNN_delta_per_step_dm_cvd.yaml'
-    path_to_config = '../configs/RNN_based_model_configs/dm_cvd_configs/validate_RNN_delta_per_step_dm_cvd.yaml'
+#    path_to_config = '../configs/RNN_based_model_configs/dm_cvd_configs/validate_RNN_delta_per_step_dm_cvd.yaml'
 
 
 #    path_to_config = '../configs/RNN_based_model_configs/dm_cvd_configs/learn_fixed_theta_RNN_delta_per_step_dm_cvd.yaml'
@@ -79,7 +78,7 @@ if __name__ == '__main__':
 
 #    path_to_config = '../configs/RNN_based_model_configs/covid_configs/RNN_delta_per_step_covid.yaml'
 #    path_to_config = '../configs/RNN_based_model_configs/covid_configs/learn_fixed_theta_RNN_delta_per_step_covid.yaml'
-#    path_to_config = '../configs/linear_baseline_configs/covid_configs/learn_fixed_theta_linear_delta_per_step_covid.yaml'
+    path_to_config = '../configs/linear_baseline_configs/covid_configs/learn_fixed_theta_linear_delta_per_step_covid.yaml'
 
 
 #    path_to_config = '../configs/RNN_based_model_configs/mimic_configs/RNN_delta_per_step_mimic.yaml'
